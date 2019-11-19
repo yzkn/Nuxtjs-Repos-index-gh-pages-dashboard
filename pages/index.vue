1bc619b04413fd7a89f5deed46b48cfc8c96058d
YA <YA-androidapp@users.noreply.github.com>
2019-11-19 10:37:09 +0900

    Remove password form

--- pages/index.vue
+++ pages/index.vue
@@ -1,62 +1,63 @@
 <template>
-  <v-content>
-    <v-container fluid fill-height>
-      <v-layout align-center justify-center>
-        <v-flex xs12 sm8 md4>
-          <material-card
-            color="success"
-            elevation="12"
-            title="Connexion"
-          >
-            <v-card-text>
-              <v-form>
-                <v-text-field type="text" v-model="username" prepend-icon="person" name="username" label="Login" :placeholder="defaultUserPassword"></v-text-field>
-                <v-text-field type="password" v-model="password" prepend-icon="lock" name="password" label="Password" :placeholder="defaultUserPassword"></v-text-field>
-              </v-form>
-            </v-card-text>
-            <v-card-actions>
-              <v-layout justify-center align-center>
-                <v-btn color="success" :disabled="isDisabled" @click.prevent="authenticate">Login</v-btn>
-              </v-layout>
-            </v-card-actions>
-          </material-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </v-content>
+    <v-content>
+        <v-container fluid fill-height>
+            <v-layout align-center justify-center>
+                <v-flex xs12 sm8 md4>
+                    <material-card color="success" elevation="12" title="Connexion">
+                        <v-card-text>
+                            <v-form>
+                                <v-text-field
+                                    type="text"
+                                    v-model="username"
+                                    prepend-icon="person"
+                                    name="username"
+                                    label="Login"
+                                    :placeholder="defaultUsername"
+                                ></v-text-field>
+                            </v-form>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-layout justify-center align-center>
+                                <v-btn
+                                    color="success"
+                                    :disabled="isDisabled"
+                                    @click.prevent="authenticate"
+                                >Login</v-btn>
+                            </v-layout>
+                        </v-card-actions>
+                    </material-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-content>
 </template>
 
 <script>
-  import { mapActions } from 'vuex'
-  import materialCard  from '~/components/material/AppCard'
+    import { mapActions } from "vuex";
+    import materialCard from "~/components/material/AppCard";
 
-  export default {
-    components: {
-      materialCard
-    },
-    data() {
-      return {
-        username: 'admin',
-        password: 'admin',
-        defaultUserPassword: 'admin'
-      }
-    },
-    computed: {
-      isDisabled() {
-        return this.username !== this.defaultUserPassword || this.password !== this.defaultUserPassword;
-      }
-    },
-    methods: {
-      ...mapActions({
-        setUsername: 'user/setUsername'
-      }),
+    export default {
+        components: {
+            materialCard
+        },
+        data() {
+            return {
+                username: "admin",
+                defaultUsername: "admin"
+            };
+        },
+        computed: {},
+        methods: {
+            ...mapActions({
+                setUsername: "user/setUsername"
+            }),
 
-      async authenticate() {
-        if (!this.isDisabled) {
-          await this.setUsername(this.defaultUserPassword);
-          this.$router.push({ path: 'dashboard' });
+            async authenticate() {
+                if (!this.username !== "") {
+                    await this.setUsername(this.username);
+                    this.$router.push({ path: "dashboard" });
+                }
+            }
         }
-      }
-    }
-  }
+    };
 </script>
