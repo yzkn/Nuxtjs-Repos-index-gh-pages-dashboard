76e422f97fd0770e8c52e1eb67a623f28266131a
YA <YA-androidapp@users.noreply.github.com>
2019-11-22 12:55:43 +0900

    minor update

--- store/repos/getters.js
+++ store/repos/getters.js
@@ -1,18 +1,10 @@
 // Copyright (c) 2019 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
 export default {
-  getUser(state) {
-    return state;
-  },
-
   page(state) {
     return state.page;
   },
 
   repos(state) {
     return state.repos;
-  },
-
-  page(state) {
-    return state.page;
   }
 }
